8694fa5262605eedb9bff16dc60ee95513e120bd
Shivam-Yadav <shivam.yadav@st.niituniversity.in>
2017-02-05 00:45:15 +0530

    adding html pages to python script

--- Python_website/flask_website.py
+++ Python_website/flask_website.py
@@ -1,12 +1,14 @@
 __author__ = 'Shivam'
-from flask import Flask #Flask is a class in flask module
-                        #__name__ is a special variable that will get name of the script
-app=Flask(__name__)  #instantiating the object Flask
+from flask import Flask, render_template #Flask is a class in flask module,
+                                         # render_template accesses html file, stores in python files and displays on requested url
+
+app=Flask(__name__)                        #__name__ is a special variable that will get name of the script
+                                           #instantiating the object Flask
 
 @app.route('/about/')   #url for localhost localhost:5000/about/in this case
 
 def about():      #defines what ur website will do
-    return "My website"
+    return render_template("about.html")
 
 @app.route('/home/')
 def home():      #defines what ur website will do
